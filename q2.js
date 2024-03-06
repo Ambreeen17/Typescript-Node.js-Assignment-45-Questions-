1f2dd5ac3cd5ab74ebd172f8f8aaf12274fd8a28
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 03:43:54 +0500

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,8 @@
+/*Q2
+Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+ */
+var friendName = "Maha";
+console.log("\"Hello ".concat(friendName, ",Would you like to join us for next trip?\""));
+friendName = "ALI";
+console.log("\"Assalamualikum ".concat(friendName, ",are you comming?\""));
