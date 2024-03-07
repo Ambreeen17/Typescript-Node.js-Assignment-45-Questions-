5b57c1ea0021be11d938db38d96f8bd625daef45
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 06:24:16 +0500

    update

---
+++ 12 Greetings/app.js
@@ -0,0 +1,6 @@
+//it will show personalize message to each friend in a list.
+var friendsname = ["Sadia", "Zahra", "Maryum"];
+for (var _i = 0, friendsname_1 = friendsname; _i < friendsname_1.length; _i++) {
+    var name_1 = friendsname_1[_i];
+    console.log("Assalamualikum ".concat(name_1, ",Eid Mubarak to you and your family"));
+}
