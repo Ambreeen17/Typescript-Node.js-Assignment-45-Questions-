97a29ede33e2cb6ac6bfdd2df2bbc2be677a0d99
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 06:12:40 +0500

    update

---
+++ 08 Number 8/app.js
@@ -0,0 +1,9 @@
+/*QUESTION #8
+You should create four lines that look like this:
+console.log(5 + 3)
+Your output should simply be four lines with the number 8 appearing once on each line.
+*/
+console.log(2 + 6);//result 8
+console.log(4 + 4);
+console.log(1 + 7);
+console.log(8 + 0);
