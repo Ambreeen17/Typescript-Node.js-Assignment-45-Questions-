a0b689e92e006e9342f448c771e3cef3356ffda0
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 06:18:48 +0500

    update

---
+++ 10 Adding Comments/app.js
@@ -0,0 +1,8 @@
+/*comments are notes in your code that computer ignores.They help people understand what your code does*/
+//AMBREEN,
+//5th March 2024
+//This program prints the name in lower case,upper case and title case.
+let Name:string="maha";
+console.log(PersonName.toLowerCase());
+console.log(PersonName.toUpperCase());
+console.log(PersonName.charAt(0).toUpperCase()+PersonName.slice(1).toLowerCase());
