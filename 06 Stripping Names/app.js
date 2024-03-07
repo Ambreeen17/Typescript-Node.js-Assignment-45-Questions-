1bc7c65844b7437f294e09680aaf9d8c740ac2f3
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 05:31:03 +0500

    update

---
+++ 06 Stripping Names/app.js
@@ -0,0 +1,8 @@
+//  '\t'=tab..it save the name with extra space and "\n"=new lines..it add new lines around the name
+ var personName: "\t\n Ambreen Rais \t\n";
+ console.log(personName);
+ console.log(personName.trim());  //.trim()..it remove the spaces and show clean up text
+ 
+ 
+ 
+ 
