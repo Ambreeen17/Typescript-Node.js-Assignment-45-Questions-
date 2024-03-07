6d2869f3c44da44d0921cc6773d6da2d0bd3c1e8
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 05:26:14 +0500

    update

---
+++ 05 Famous Quote2/app.js
@@ -0,0 +1,5 @@
+//save the author name in variable
+var famous_person = "Quaid-e-Azam Muhammed Ali Jinnah ";
+//then create quote in another variable
+var message = "".concat(famous_person, " once said,\"There are two powers in the world, one is the sword and the other is the pen.\"");
+console.log(message);
