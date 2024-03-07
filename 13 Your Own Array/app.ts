21bc7ed184d65e0fb47aafbe6cdad176d5ef3e27
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 06:32:40 +0500

    update

--- 13 Your Own Array/app.ts
+++ 13 Your Own Array/app.ts
@@ -1,4 +1,4 @@
-let transports:string[]=["Bmw","Audi","Ferrari","Ducati","Ducati"];
+let transports:string[]=["Bmw","Audi","Ferrari","Ducati"];
 transports.forEach(transport =>{
     console.log(`I would like to own a ${transport}.`);
 });
