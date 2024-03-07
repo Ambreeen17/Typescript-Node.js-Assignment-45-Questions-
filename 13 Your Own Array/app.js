8bff75368cd59b737345836d2a17c2fb7eadac8c
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 06:42:20 +0500

    update

---
+++ 13 Your Own Array/app.js
@@ -0,0 +1,5 @@
+//Array help group related items.This way you can talk about each one using loop. 
+var transports = ["Bmw", "Audi", "Ferrari", "Ducati panigale"];
+transports.forEach(function (transport) {
+    console.log("I would like to own a ".concat(transport, "."));
+});
