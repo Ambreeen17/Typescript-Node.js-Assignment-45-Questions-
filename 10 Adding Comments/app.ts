9c0d553b2eb6484a6061c5c57fb1c15b83ba9018
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 06:17:55 +0500

    update

---
+++ 10 Adding Comments/app.ts
@@ -0,0 +1,7 @@
+//AMBREEN RAIS,
+//5th March 2024
+//This program prints the name in lower case,upper case and title case.
+let Name:string="maha";
+console.log(PersonName.toLowerCase());
+console.log(PersonName.toUpperCase());
+console.log(PersonName.charAt(0).toUpperCase()+PersonName.slice(1).toLowerCase());
