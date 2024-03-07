435233c481769aae30f0f8938e56bc2168561022
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 05:15:39 +0500

    Create app.js

---
+++ 03 Name Cases/app.js
@@ -0,0 +1,5 @@
+//save the name in variable
+var namePerson = "maryum";
+console.log(namePerson.toLowerCase()); //result will be in lower case "maryum"
+console.log(namePerson.toUpperCase()); //result will be in upper case "MARYUM"
+console.log(namePerson.charAt(0).toUpperCase() + namePerson.slice(1).toLowerCase()); //result will be in title case "Maryum"
