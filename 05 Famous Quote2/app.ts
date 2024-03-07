5f01d88f5772cd10f24f80ef9ca343810e2b30c4
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 05:24:11 +0500

    update

---
+++ 05 Famous Quote2/app.ts
@@ -0,0 +1,3 @@
+let famous_person:string="Quaid-e-Azam Muhammed Ali Jinnah ";
+let message: string=`${famous_person} once said,"There are two powers in the world, one is the sword and the other is the pen."`;
+console.log(message);
