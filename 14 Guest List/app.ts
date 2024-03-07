67fa3df50f7f2cc6f56eda15ac2f3a95fabb0fcd
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 06:45:43 +0500

    update

---
+++ 14 Guest List/app.ts
@@ -0,0 +1,5 @@
+ */
+ let  Guests:String[]=["Wania Rais","Aaina Khan","Syeda Fatima","Summaiya Ali"];
+ Guests.forEach(Guest =>{
+     console.log(`Dear ${Guest}!You are invited to a birthday extravaganza! Be part of the festivities as we celebrate Azeem's special day in style. `);
+ } );
