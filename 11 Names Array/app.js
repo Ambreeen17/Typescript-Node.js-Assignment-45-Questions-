6d06fdcdb87d699195db93f4ea7efd8a1257127f
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 06:21:38 +0500

    update

---
+++ 11 Names Array/app.js
@@ -0,0 +1,6 @@
+//An array is like a list of items.It can help us to store multiple items in one variable and access them one by one. 
+var friend_Name = ["Sadia", "Zahra", "Maryum"];
+for (var i = 0; i < friend_Name.length; i++) {
+    console.log(friend_Nam
+                e[i]);
+}
