8658aec9fa99d0c5752c2e4be65d47c044a7df95
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-07 05:13:48 +0500

    Create app.ts

---
+++ 03 Name Cases/app.ts
@@ -0,0 +1,4 @@
+let Person_Name:string="maryum";
+console.log(Person_Name.toLowerCase());
+console.log(Person_Name.toUpperCase());
+console.log(Person_Name.charAt(0).toUpperCase()+Person_Name.slice(1).toLowerCase());
