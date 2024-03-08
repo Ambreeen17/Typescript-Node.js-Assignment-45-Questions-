adb0cc58a83a45973cfd9e30a1f5fbaea8a96f36
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-08 10:17:45 +0500

    update

---
+++ 21 Object/aap.js
@@ -0,0 +1,2 @@
+var book = { title: "Neutral Thinking", author: "Khuwaja Shamsuddin Azeemi", yearPublished: 2009 };
+console.log("Book Info:".concat(book.title, " by ").concat(book.author, ",published in ").concat(book.yearPublished));
