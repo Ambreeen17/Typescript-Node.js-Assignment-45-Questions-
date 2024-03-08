77b61653b2de280882afb3427979a046eddef3c9
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-08 10:15:53 +0500

    update

---
+++ 21 Object/app.ts
@@ -0,0 +1,2 @@
+let book:{title:string;author:string;yearPublished:number}={title:"Neutral Thinking",author:"Khuwaja Shamsuddin Azeemi",yearPublished:2009};
+console.log(`Book Info:${book.title} by ${book.author},published in ${book.yearPublished}`);
