308464c8664b48dcc805891340cdc1d6c371f970
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-08 10:10:32 +0500

    update

---
+++ 20 List Array/app.js
@@ -0,0 +1,4 @@
+var Language = ["Urdu", "English", "Persian", "Arabic", "French", "Russian", "Chinese", "Japanese", "Hindi"];
+Language.forEach(function (Language) {
+    console.log(Language);
+});
