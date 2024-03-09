f425c7817cb66e9a34433d6f7aee203f0465293d
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-09 18:23:03 +0500

    update

---
+++ 22 Intentional Error/app.ts
@@ -0,0 +1,3 @@
+let Transports:string[]=["Bmw","Audi","Ferrari"];
+console.log(Transports[4]);//international error because index 4 is out of bounds
+Transports[2]="Audi";//correcting the error by accessing a valid index
