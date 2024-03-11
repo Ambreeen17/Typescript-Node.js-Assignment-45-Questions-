72af65d96f92f46b9435e8a8e92fa41a9e228c24
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-12 01:19:58 +0500

    update

---
+++ 23 Conditional Test/app.js
@@ -0,0 +1,30 @@
+//Test 1 and 2
+var number = 27;
+console.log("Is number equal to =='27'?I predict True");
+console.log(number == 27);
+console.log("Is number not equal to =='27'? I predict False");
+console.log(number != 27);
+//Test 3 and 4
+var country = "Pakistan";
+console.log("Is your country name =='Pakistan'?I predict True");
+console.log(country == 'Pakistan');
+console.log("Is your counry name =='Saudia Arab'?I predict False");
+console.log(country == 'Saudia Arab');
+//Test 5 and 6
+var IDNumber = 1217;
+console.log("Is roll number '1217'?I predict True");
+console.log(IDNumber == 1217);
+console.log("Is roll number '1219'?I predict False");
+console.log(IDNumber == 1219);
+//Test 7 and 8
+var car = "Toyota";
+console.log("Is car =='Toyota'?I predict True");
+console.log(car == 'Toyota');
+console.log("Is car =='Civic'?I predict False");
+console.log(car == 'Civic');
+//Test 9 and 10
+var colour = "Red";
+console.log("Is colour =='red''?I predict True");
+console.log(colour == 'Red');
+console.log("Is colour =='blue'?I predict False");
+console.log(colour == 'blue');
