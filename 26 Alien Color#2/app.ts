9e7c47f61158fc1f9906309b5ce220db564ae40e
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-12 01:29:47 +0500

    update

---
+++ 26 Alien Color#2/app.ts
@@ -0,0 +1,16 @@
+//version that passes
+let alien_colour="green";
+if(alien_colour =="green"){
+    console.log("You just earned 5 points for shooting the alien");
+} 
+else
+ {
+console.log("You just earned 10 points.");}
+//version that 
+//version that runs else block
+alien_colour="yellow";
+if(alien_colour =="green"){
+    console.log("You just earned 5 points for shooting the alien!");
+}else{
+    console.log("You just earned 10 points.")
+}
