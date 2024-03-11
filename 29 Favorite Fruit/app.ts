737d11f0b171d206ddf8c45300c8439b6f4c2a9c
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-12 04:00:53 +0500

    update

---
+++ 29 Favorite Fruit/app.ts
@@ -0,0 +1,17 @@
+let favoriteFruit:string[]=["Orange","Grapes","Strawberry","Blue Berry","Pine Apple"];
+if (favoriteFruit.includes("Grapes")){
+    console.log("You really like Grapes!");
+}
+if(favoriteFruit.includes("Orange")){
+    console.log("You really like Orange!");
+}
+if (favoriteFruit.includes("Strawberrye")){
+    console.log("You really like Strawberry!");
+}
+if(favoriteFruit.includes("Blue Berry")){
+    console.log("You really like Blue Berry!");
+}
+if(favoriteFruit.includes("Pine Apple")){
+    console.log("You really like Pine Apple!");
+}
+
