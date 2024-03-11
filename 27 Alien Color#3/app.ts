4a235be3dc87848aeee34e876a5d7ca5c0452b5f
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-12 01:34:45 +0500

    update

---
+++ 27 Alien Color#3/app.ts
@@ -0,0 +1,27 @@
+//GREEN ALIEN VERSION
+let alien_colour="green";
+if(alien_colour =="green"){
+    console.log("You  earned 5 points. ");
+} else if (alien_colour=="yellow"){
+console.log("You just earned 10 points.");}
+else  if
+    (alien_colour=="red"){
+        console.log("You just earned 15 points.");
+    }
+//YELLOW ALIEN
+alien_colour="yellow";
+if(alien_colour=="green"){
+    console.log("You earned 5 points"); 
+}else if (alien_colour=="yellow"){
+console.log("You just earned 10 points");}
+else if (alien_colour=="red"){
+    console.log("You just earned 15 points");
+}
+//RED ALIEN
+alien_colour="red";
+if(alien_colour=="green"){
+    console.log("You earned 5 points"); 
+}else if (alien_colour=="yellow"){
+console.log("You just earned 10 points");}
+else if (alien_colour=="red"){
+    console.log("You just earned 15 points");}
