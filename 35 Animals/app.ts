e43ea04f277aeadb2a5531999c649234845935c9
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-13 00:56:40 +0500

    update

---
+++ 35 Animals/app.ts
@@ -0,0 +1,5 @@
+let animals:string[]=["Cat","Turtle","Hen"];
+animals.forEach(animal=>{
+    console.log(`A ${animal} would make a great pet.`);
+});
+console.log("Any of these animals would make a great pet.");
