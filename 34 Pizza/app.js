23994e8917dc2370fd3f1b828af2a48e88ee740e
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-13 00:41:06 +0500

    update

---
+++ 34 Pizza/app.js
@@ -0,0 +1,5 @@
+var pizzas = ["Marghweota", "Fajita", "BBQ Chicken", "Afghani Tikka"];
+pizzas.forEach(function (pizza) {
+    console.log("I like ".concat(pizza, " pizza ."));
+});
+console.log("I really like pizza!");
