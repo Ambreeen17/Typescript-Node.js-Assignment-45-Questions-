f9ca4e428fa5b0180e34f391ba131832ff2a4870
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-13 01:13:43 +0500

    update

---
+++ 36 T-Shirt(function)/app.ts
@@ -0,0 +1,5 @@
+function make_shirt(size:string,message:string)
+ {
+    console.log(`Making a ${size} t-shirt with the message "${message}"  printed on it.`);
+ }
+ make_shirt("medium","Sky is the limit");
