dd7a0b7b06e58cd6e42462ded3da9425f75cf972
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-12 07:32:54 +0500

    update

---
+++ 31 No User/app.ts
@@ -0,0 +1,9 @@
+Add an if test to Exercise 28 to make sure the list of users is not empty.*/
+let user_names:string[]=[];
+ if (user_names.length === 0){
+    console.log("We need to find some users!");
+ }else{
+    //Greet users
+    console.log
+ }
+  //removing all user names ensures the message "we need to find some users!" is printed
