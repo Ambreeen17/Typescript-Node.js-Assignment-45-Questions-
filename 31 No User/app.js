fa4a00547f8d061802faaa0865f3e33ab52384fb
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-12 07:33:49 +0500

    update

---
+++ 31 No User/app.js
@@ -0,0 +1,9 @@
+var user_names = [];
+if (user_names.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    //Greet users
+    console.log;
+}
+// removing all user names ensures the message "we need to find some users!" is printed
