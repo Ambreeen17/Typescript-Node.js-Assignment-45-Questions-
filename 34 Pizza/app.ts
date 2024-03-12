6e1d43f3570f22102e36d504cf5c76d586092302
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-13 00:40:02 +0500

    update

---
+++ 34 Pizza/app.ts
@@ -0,0 +1,5 @@
+let pizzas:string[]=["Marghweota","Fajita","BBQ Chicken","Afghani Tikka"];
+ pizzas.forEach(pizza=> {
+    console.log(`I like ${pizza} pizza .`);
+ });
+ console.log("I really like pizza!");
