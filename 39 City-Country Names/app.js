ba5bd77f5d5162b784bae835fe7bba0c17940ab2
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-14 01:34:31 +0500

    update

---
+++ 39 City-Country Names/app.js
@@ -0,0 +1,8 @@
+function city_country(city, country) {
+    return "".concat(city, ",").concat(country);
+}
+console.log(city_country("Islamabad", "Pakistan"));
+console.log(city_country("London", "England"));
+console.log(city_country("Baghdad", "Iraq"));
+console.log(city_country("Istanbul", "Turkey"));
+console.log(city_country("New York", "USA"));
