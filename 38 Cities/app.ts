8c10482136063400e69bb2c465cab26c689e993b
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-14 01:26:35 +0500

    update

---
+++ 38 Cities/app.ts
@@ -0,0 +1,7 @@
+function describe_city(city:string,country:string="Saudia Arab") {
+  console.log(`${city} is in ${country}`)  ;
+} 
+describe_city("Makkah");
+describe_city("Madina");
+describe_city("Jaddah");
+describe_city("Karachi","Pakistan");
