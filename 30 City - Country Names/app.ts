7930977e2f6eebea13a9d90f3c577d49f93382fd
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-14 01:29:13 +0500

    update

---
+++ 30 City - Country Names/app.ts
@@ -0,0 +1,8 @@
+function city_country(city: string,country:string) :string{
+    return `${city},${country}`;
+}
+console.log(city_country("Islamabad","Pakistan"));
+console.log(city_country("London","England"));
+console.log(city_country("Baghdad","Iraq"));
+console.log(city_country("Istanbul","Turkey"));
+console.log(city_country("New York","USA"));
