5ea8ae2e57abce700b3ee7099debe372dee110a7
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-15 13:31:42 +0500

    update

---
+++ 41 Magicians/app.ts
@@ -0,0 +1,7 @@
+let magicians:string[]=["David Blaine","Darren Brown","Criss Angel","David Copperfield"];
+function show_magicians(magicians:string[]) {
+    magicians.forEach(magicians=>{
+        console.log(magicians);
+    });
+}
+show_magicians(magicians);
