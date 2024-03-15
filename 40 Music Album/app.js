1fbaef2ed561eb960a80a4f34c050445bb18a15a
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-15 13:29:00 +0500

    app.js

--- 40 Music Album/app.js
+++ 40 Music Album/app.js
@@ -1,13 +1,10 @@
-function favorite_albums(artist:string,title:string,tracks?:number) {
-    let album={artist,title,tracks};
-    if (tracks){
-        album['tracks']=tracks;
- }
- return album;
+function favorite_albums(artist, title, tracks) {
+    var album = { artist: artist, title: title, tracks: tracks };
+    if (tracks) {
+        album['tracks'] = tracks;
+    }
+    return album;
 }
-console.log(favorite_albums("Sami Yusuf","Al Mu'allim",5));
-console.log(favorite_albums("Atif Aslam","Ehsaas",7));
-console.log(favorite_albums("Abida Perven","Heer",12));
-
-
-
+console.log(favorite_albums("Sami Yusuf", "Al Mu'allim", 5));
+console.log(favorite_albums("Atif Aslam", "Ehsaas", 7));
+console.log(favorite_albums("Abida Perven", "Heer", 12));
