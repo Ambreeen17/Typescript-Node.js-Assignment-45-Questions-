f93889319f4eadef4ff740db1dcf59a57096a816
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-17 22:21:22 +0500

    Delete DAY 16/46 Laptop Objects directory

--- DAY 16/46 Laptop Objects/app.js
+++
@@ -1,9 +0,0 @@
-var laptop = {
-    make: "Dell",
-    model: "X16 R1",
-    year: 2024,
-    describe: function () {
-        console.log("This laptop is a ".concat(this.year, " ").concat(this.make, " ").concat(this.model, "."));
-    }
-};
-laptop.describe();
--- DAY 16/46 Laptop Objects/app.ts
+++
@@ -1,9 +0,0 @@
-let laptop={
-    make:"Dell",
-    model:"X16 R1",
-    year:  2024,
-    describe:function(){
-        console.log(`This laptop is a ${this.year} ${this.make} ${this.model}.`);
-    }
-};
-laptop.describe();
