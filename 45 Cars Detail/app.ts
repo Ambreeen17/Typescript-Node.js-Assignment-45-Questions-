3f22ec50ceda19ab5400765bd4a480ca6333ebce
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-17 23:28:56 +0500

    Update

--- 45 Cars Detail/app.ts
+++ 45 Cars Detail/app.ts
@@ -1,14 +1,9 @@
-let car = {
-    manufacturer: string;
-    model: string;
-    [key: string]: any; // Allow any additional properties
-}
-
-function  Car_make(manufacturer: string, model: string, ...options: [string, any][]): Car {
-    let car: Car = { manufacturer, model };
+function  Car_make(manufacturer: string, model: string, ...options: [string, any][]): Object {
+    let Car= { manufacturer, model };
     options.forEach(([key, value]) => car[key] = value);
     return car;
 }
 
 console.log(Car_make("Toyota", "Hilux", ["color", "red"], ["year", 2022]));
-console.log(make_car("Ford", "Fiesta", ["color", "black"], ["sunroof", true]));
+console.log(Car_make("Ford", "Fiesta", ["color", "black"], ["sunroof", true]));
+
