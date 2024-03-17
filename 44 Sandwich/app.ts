4fcc29b0497ef3a9246b534be36502930e961089
Ambreen Rais <162341214+Ambreeen17@users.noreply.github.com>
2024-03-17 23:13:27 +0500

    update

---
+++ 44 Sandwich/app.ts
@@ -0,0 +1,10 @@
+function make_sandwich(...items:string[]) {
+    console.log(`Making a sandwich with: ${items.join(',')}.`);
+    
+}
+make_sandwich("chicken","cheese");
+
+make_sandwich("egg","mayo","special Spicy Sauce");
+
+make_sandwich("green Sauce","cheesy chicken Spread","lettuce");
+
